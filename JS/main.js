e683c5becff69e70361fbcf87383552d078cdaad
Xavvir <leandroxm1997@hotmail.com>
2023-06-27 23:29:15 -0300

    finalização navbar

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,44 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector (mobileMenu);
+        this.navList = document.querySelector (navList);
+        this.navLinks = document.querySelectorAll (navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
     }
-})
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+          link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 5 + 0.3
+              }s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);  
+
+mobileNavbar.init();
